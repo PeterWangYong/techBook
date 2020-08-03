d52bb7d657f36cc1fdb58aad527e391fdec61cc9
peter <763874832@qq.com>
2020-08-03 15:29:58 +0800

    message

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
 
+# 自定义提交信息
 message=$1
-if [ -z message ];then message='deploy';fi
-echo $message
 
 # 确保脚本抛出遇到的错误
 set -e
 
+# 源文档发布
 git add -A
 git commit -m $message
 git push -f https://github.com/PeterWangYong/frontBook.git master
+git push -f https://gitee.com/PeterWangYong/frontBook.git master
 
 # 生成静态文件
 npm run docs:build
@@ -17,15 +18,10 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# 静态页面发布
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:PeterWangYong/peterwangyong.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 发布到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/PeterWangYong/frontBook.git master:gh-pages
