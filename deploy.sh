f117d4b57622d3a80402dc290c5ffb68ad079414
peter <763874832@qq.com>
2020-08-03 15:54:15 +0800

    message

--- deploy.sh
+++ deploy.sh
@@ -25,3 +25,4 @@ git commit -m 'deploy'
 
 # 发布到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/PeterWangYong/frontBook.git master:gh-pages
+git push -f https://gitee.com/PeterWangYong/frontBook.git master:gh-pages
